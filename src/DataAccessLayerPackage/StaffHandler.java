b6922a9b6a0b1c6644044da7f7ca26ecd89e3764
JozehanGrobler <jozehan.grobies@gmail.com>
2018-10-13 00:11:15 +0200

    DAL Singleton Pattern v1

--- src/DataAccessLayerPackage/StaffHandler.java
+++ src/DataAccessLayerPackage/StaffHandler.java
@@ -5,13 +5,36 @@
  */
 package DataAccessLayerPackage;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Tyrone
  */
 public class StaffHandler {
 
-    public StaffHandler() {
+    private static StaffHandler instance = new StaffHandler();
+    private StaffHandler() {}
+    public static StaffHandler getInstance(){
+        return instance;
+    }
+    public static void openConnection()
+    {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(StaffHandler.class.getName()).log(Level.ALL.SEVERE, null, ex);
+        }
+            String url="jdbc:mysql://localhost:3306/mysql?zeroDateTimeBehavior=convertToNull";
+        try { 
+            Connection cn = (Connection) DriverManager.getConnection(url,"root","");
+        } catch (SQLException ex) {
+            Logger.getLogger(StaffHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
